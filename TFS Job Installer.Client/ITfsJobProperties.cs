008bd97ac543e979d67154efddde6168804ff553
Gordon Beeming <gordon@beeming.co.za>
2015-08-04 00:22:53 +0200

    init commit

---
+++ TFS Job Installer.Client/ITfsJobProperties.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.TeamFoundation.Framework.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS_Job_Installer.Client
+{
+    public interface ITfsJobProperties
+    {
+        TeamFoundationJobDefinition JobDefinition { get; set; }
+    }
+}
---
+++ TFS Job Installer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS_Job_Installer
+{
+    class Program
+    {
+        const string ToolsPath = @"C:\Program Files\Microsoft Team Foundation Server 14.0\Tools";
+
+        static void Main(string[] args)
+        {
+            var appExe = new FileInfo(Assembly.GetExecutingAssembly().Location);
+            if (string.Compare(appExe.Directory.FullName.TrimEnd('\\'), ToolsPath, StringComparison.InvariantCultureIgnoreCase) != 0)
+            {
+                Console.WriteLine($"This tool can only be run from {ToolsPath}.");
+            }
+        }
+    }
+}
